3b47f4879801abd0e8f887fb14d126f0dd46a925
Yuichi Sugiyama <oogFranz@gmail.com>
2018-08-17 23:01:10 +0900

    BoardとSquareコンポーネントを作成

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Square from './Square';
+
+export default class Board extends Component {
+  render() {
+    return (
+      <div className="Board">
+          <Square />
+          <Square />
+      </div>
+    );
+  }
+}
