74427c1d67dadbe1a17c6a63248c03abe0c07c51
Yuichi Sugiyama <oogFranz@gmail.com>
2018-08-27 23:34:54 +0900

    When you click a square, the square's value will be incremented.

--- src/Board.js
+++ src/Board.js
@@ -2,11 +2,47 @@ import React, { Component } from "react";
 import Square from "./Square";
 
 export default class Board extends Component {
+  constructor(props) {
+    super(props);
+    const squares = Array.from(new Array(4), () => new Array(4).fill(0));
+    this.state = {
+      squares: squares,
+    };
+  }
+
+  handleClick(i, j) {
+    const squares = this.state.squares.slice();
+    squares[i][j] += 1;
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i, j) {
+    return (
+      <Square
+        value={this.state.squares[i][j]}
+        onClick={() => this.handleClick(i, j)}
+      />
+    );
+  }
+
+  renderRow(row) {
+    return (
+      <div className="board-row">
+        {this.renderSquare(0, row)}
+        {this.renderSquare(1, row)}
+        {this.renderSquare(2, row)}
+        {this.renderSquare(3, row)}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Board">
-        <Square />
-        <Square />
+        {this.renderRow(0)}
+        {this.renderRow(1)}
+        {this.renderRow(2)}
+        {this.renderRow(3)}
       </div>
     );
   }
--- src/Square.js
+++ src/Square.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Square extends Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
   }
 }
